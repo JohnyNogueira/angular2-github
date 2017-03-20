7bcccaf2cf95aa5a6067645a40611e5702c8c747
luancurti <luancurti@gmail.com>
2017-03-20 15:54:16 -0300

    Fix test search users using when stable to wait the return of the observable

--- src/app/home/user-search.component.spec.ts
+++ src/app/home/user-search.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
@@ -9,47 +9,42 @@ import { Observable } from 'rxjs/Rx';
 import { UserSearchComponent } from './user-search.component';
 import { UserFilterPipe } from './user-filter.pipe';
 import { UserService } from './user.service';
-import { MockUser } from './mocker-user';
+import { MockUserService } from './user.service.mock';
+import { IUser } from './user';
+import { users } from './user-data.mock';
 
 describe('UserSearchComponent', () => {
   let userSearchComponent: UserSearchComponent;
   let userService: UserService;
   let userSearchComponentFixture: ComponentFixture<UserSearchComponent>;
-  const Users = [
-    {
-      name: 'Luan Curti', followers: 13, following: 15, email: 'luancurti@gmail.com',
-      avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=3', bio: ''
-    },
-    {
-      name: 'Lionel Messi', followers: 13000000, following: 123, email: 'lionel@gmail.com',
-      avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=3', bio: ''
-    }
-  ];
+  let element: HTMLElement;
 
-  beforeEach((() => {
+  beforeEach(async((() => {
     TestBed.configureTestingModule({
       declarations: [
         UserSearchComponent,
         UserFilterPipe
       ],
       providers: [
-        { provide: UserService, useClass: MockUser }
+        { provide: UserService, useClass: MockUserService }
       ],
       imports: [ RouterTestingModule, FormsModule ]
-    }).compileComponents();
+    });
 
-    userSearchComponentFixture = TestBed.createComponent(UserSearchComponent);
+    TestBed.compileComponents()
+      .then(() => {
+        userSearchComponentFixture = TestBed.createComponent(UserSearchComponent);
+        userSearchComponent = userSearchComponentFixture.componentInstance;
+        element = userSearchComponentFixture.debugElement.nativeElement;
+        userService = TestBed.get(UserService);
+      });
+  })));
 
-    userSearchComponent = userSearchComponentFixture.componentInstance;
-
-    userService = TestBed.get(UserService);
-
-    spyOn(userService, 'getUsers').and.returnValue(Observable.from(Users));
-  }));
-
-  it('should set the user', () => {
+  it('should set the users property after initialize', async(() => {
     userSearchComponent.ngOnInit();
 
-    expect(userSearchComponent.users).toBe(Users);
-  });
+    userSearchComponentFixture.whenStable().then(() => {
+      expect(userSearchComponent.users).toEqual(users);
+    });
+  }));
 });
